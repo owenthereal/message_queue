9950b1521719de640a376bf7e2d97b7cb9e6cb33
Jingwen Owen Ou <jingweno@gmail.com>
2013-09-04 18:09:43 -0700

    Always return true for in memory publish

--- lib/message_queue/adapters/memory/publisher.rb
+++ lib/message_queue/adapters/memory/publisher.rb
@@ -6,5 +6,6 @@ class MessageQueue::Adapters::Memory::Connection::Publisher < MessageQueue::Publ
   def publish(object, options = {})
     changed
     notify_observers(dump_object(object), default_options.merge(options))
+    true
   end
 end
